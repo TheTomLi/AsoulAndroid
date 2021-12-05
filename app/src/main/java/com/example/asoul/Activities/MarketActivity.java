096276076ce8fd78d09a2cf96342cf7b5fb01b4f
TheTomLi <90334025+TheTomLi@users.noreply.github.com>
2021-12-04 23:06:21 -0500

    First commit

---
+++ app/src/main/java/com/example/asoul/Activities/MarketActivity.java
@@ -0,0 +1,14 @@
+package com.example.asoul.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.asoul.R;
+
+public class MarketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market);
+    }
+}
